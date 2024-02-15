fcc2e7d637feffbc6247c46a9b506c39bb71fa20
bryanvengwa <bmvengwa@gmail.com>
2024-02-15 15:01:58 +0200

    banner update

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -61,11 +61,7 @@ export default function Card(){
 </div>
 
 
-<p className="mt-5">This card component is part of a larger, open-source library of Tailwind CSS components. Learn
-    more
-    by going to the official <a className="text-blue-600 hover:underline"
-        href="#" target="_blank">Flowbite Documentation</a>.
-</p>
+
 </div>
         </div>
     )
