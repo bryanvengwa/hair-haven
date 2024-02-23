310d801952a930e43cc5a0ed30b1cf2bfcae689a
bryanvengwa <bmvengwa@gmail.com>
2024-02-23 14:05:05 +0200

    map init

--- src/components/ContactHeader.tsx
+++ src/components/ContactHeader.tsx
@@ -4,25 +4,33 @@ import '@/scss/contact.scss'
 
 function ContactHeader() {
   return (
-    <div className='contact-container1' >
-      <div className=" flex flex-col gap-3 items-center ">
+    <div className='contact-container1 ' >
+
+        
+      <div className="contact-header  flex flex-col gap-3 items-center ">
        
           <FaPhone className="phone-icon" style={{ transform: 'rotate(90deg)' }} />
         <h3>PHONE</h3>
         <p>+01-3-8888-6868</p>
       </div>
+
+
       <div className="contact-container flex flex-col gap-3 items-center ">
        
        <FaRegEnvelope className="phone-icon" />
      <h3>PHONE</h3>
      <p>+01-3-8888-6868</p>
    </div>
+
+
       <div className="contact-container flex flex-col gap-3 items-center ">
        
           <FaMapMarkerAlt className="phone-icon"  />
         <h3>PHONE</h3>
         <p>+01-3-8888-6868</p>
       </div>
+
+
       <div className="contact-container flex flex-col gap-3 items-center ">
        
           <FaRegClock className="phone-icon" />
