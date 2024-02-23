a0ec2e21b4f1f9ed1d7ec973fcffdb7e0b597be2
bryanvengwa <bmvengwa@gmail.com>
2024-02-23 10:35:42 +0200

    added first dynamic props to the blog section

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -3,13 +3,22 @@ import bannerImg from '../../public/images/blog1.jpg'
 import Image from 'next/image'
 import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
 import { faCalendar, faCalendarTimes,  faCalendarCheck,  } from '@fortawesome/free-solid-svg-icons';
-import { faComment  } from '@fortawesome/free-solid-svg-icons';
-
+// import { faComment  } from '@fortawesome/free-solid-svg-icons';
+import { FaRegCalendar , FaRegComment} from 'react-icons/fa';
 
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default function BlogCard() {
+interface blogProps {
+  title : string;
+  date : string;
+  comments : number;
+  description : string;
+  
+}
+
+
+export default function BlogCard({title, date, comments, description}:blogProps) {
   return (
     <div>
               <div className='card product-card blog-card ' >
@@ -17,19 +26,19 @@ export default function BlogCard() {
             <Image src={bannerImg} alt='product' />
             
            </div>
-           <div className="body flex flex-col gap-3 p-2 pt-3 ">
-                <div  className="flex-container ">
+           <div className="body flex flex-col gap-3  pt-3 ">
+                <div  className="flex-container flex gap-10 ">
                <div className='flex flex-row  gap-2 ' >
-               <FontAwesomeIcon icon={faCalendarAlt} />
-                    <h4>May 4,2019</h4>
+               <FaRegCalendar color='#b2b2b2' />
+                    <h3>{date}</h3>
                </div>
                  <div className='flex flex-row gap-2' >
-                 <FontAwesomeIcon icon={faComment}  />
-                    <h4>5</h4>
+                 <FaRegComment  color='#b2b2b2' />
+                    <h3> {comments} </h3>
                  </div>
                 </div>
-                <h4>Your Guide to Smoother, Sleeker Hair</h4>
-                <p>Sed quia non numquam modi tempora indunt ut labore et dolore magnam aliquam quaerat</p>
+                <h4>{ title }</h4>
+                <p> {description} </p>
            </div>
         </div>
     </div>
