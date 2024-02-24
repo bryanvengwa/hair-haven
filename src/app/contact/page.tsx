ea9ec0a39d1085478c4a32d95e382363dff77980
bryanvengwa <bmvengwa@gmail.com>
2024-02-24 12:56:47 +0200

    finished contact page footer formm

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -16,6 +16,7 @@ export default function page() {
         </div>
         <br />
         <ContactHeader />
+        
         <ContactForm />
     </div>
   )
--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
+import '@/scss/contact-form.scss'
+
 
 export default function ContactForm() {
   return (
-    <form action="">
-        <div className="flex-container">
-            <input type="text" name='name' placeholder='Your name' />
-            <input type="email" name='email' placeholder='Your email' />
+    <form className="contact-form" action="">
+        <div className="flex-container flex-container-flex">
+            <input type="text" name='name'  placeholder='Your name'  required />
+            <input type="email" name='email' placeholder='Your email' required />
         </div>
         <div className="flex-container">
-            <textarea name="meassage" id="" ></textarea>
+            <textarea name="meassage" id="" placeholder='Your message'  required ></textarea>
         </div>
         <div className="flex-container">
+        </div>
             <button type='submit' >SEND MESSSAGE</button>
-        </div>
 
     </form>
   )
