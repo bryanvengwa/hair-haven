df0ac45ece107508982f11ed1f5453fecfe28518
bryanvengwa <bmvengwa@gmail.com>
2024-02-25 03:40:09 +0200

    removed slick error

--- src/components/SliderComponent.tsx
+++ src/components/SliderComponent.tsx
@@ -1,38 +1,38 @@
-import React from "react";
-import Slider from "react-slick";
+// import React from "react";
+// import Slider from "react-slick";
 
-function SimpleSlider() {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1
-  };
-  return (
-    <div className="slider-container">
-      <Slider {...settings}>
-        <div>
-          <h3>dfghjk</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-      </Slider>
-    </div>
-  );
-}
+// function SimpleSlider() {
+//   const settings = {
+//     dots: true,
+//     infinite: true,
+//     speed: 500,
+//     slidesToShow: 1,
+//     slidesToScroll: 1
+//   };
+//   return (
+//     <div className="slider-container">
+//       <Slider {...settings}>
+//         <div>
+//           <h3>dfghjk</h3>
+//         </div>
+//         <div>
+//           <h3>2</h3>
+//         </div>
+//         <div>
+//           <h3>3</h3>
+//         </div>
+//         <div>
+//           <h3>4</h3>
+//         </div>
+//         <div>
+//           <h3>5</h3>
+//         </div>
+//         <div>
+//           <h3>6</h3>
+//         </div>
+//       </Slider>
+//     </div>
+//   );
+// }
 
-export default SimpleSlider;
+// export default SimpleSlider;
