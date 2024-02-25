f6b9aa5b6fd8e7ed8f964090bf9d04312cc3d661
bryanvengwa <bmvengwa@gmail.com>
2024-02-25 03:29:40 +0200

    added nav dropdown

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,14 +6,24 @@ import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import Navlinks from '@/components/Navlinks'
 import { useState } from 'react';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { FaUser } from 'react-icons/fa';
+import Link from 'next/link';
+
 
 
 
 export default function Header() {
 const [isOpen , setIsOpen] = useState(false)
+const [open , setOpen] = useState(false)
 
-function toggle(){
-  setIsOpen(!isOpen)
+function toggle(number :  string){
+  if(number === 'menu'){
+
+    setIsOpen(!isOpen)
+  }else{
+    setOpen(!open)
+  }
 }
 
 
@@ -25,7 +35,7 @@ function toggle(){
       <Image src={logo} alt='logo' objectFit='cover' height={'50'} />
 
       </div>
-          <div onClick={toggle} className='menu'>
+          <div onClick={()=>toggle('menu')} className='menu'>
             <div className='bar ' ></div>
             <div className='bar ' ></div>
             <div className='bar ' ></div>
@@ -55,7 +65,7 @@ function toggle(){
     </div>
 
     <div className={`slide-menu ${ isOpen ? '' : 'closed-menu' } `} >
-    <div className='logo-container ' >
+    <div className='logo-container mt-10 flex items-center  justify-center' >
       <Image src={logo} alt='logo' objectFit='cover' height={'50'} />
 
       </div>
@@ -77,9 +87,48 @@ function toggle(){
   
   <p>Item : <span>$150.00</span></p>
       </div>
+<div className="middle-container flex items-center justify-center pt-1">
+<div className="country-container flex items-center gap-3">
+            <div className="line"></div>
+                <p  className=' text-3xl' >🇺🇸</p>
+                <p>English</p>
+                {/* <FontAwesomeIcon icon={faChevronDown} /> */}
+                <div className="line"></div>
+                <div className="login-container flex items-center gap-1">
+                <FaUser/> <p>Login</p>
+
+            </div>
+
+            </div>
+</div>
+            <div className="menu-section pt-4">
+
+            <div className="menu-container pt-3">
+            <div onClick={()=>toggle('menu2')} className='menu'>
+            <div className='bar ' ></div>
+            <div className='bar ' ></div>
+            <div className='bar ' ></div>
+
+     </div>
+            </div>
+            <ul className={`pt-2 ${ open ? 'hide-ul' :'' }`}>
+              <li><Link href={'/'} >HOME</Link></li>
+              <li><Link href={'/shop'}>SHOP</Link></li>
+              <li><Link href={'/blog'}>BLOG</Link></li>
+              <li><Link href={'/contact'}>CONTACT</Link></li>
+              <li  className='drop' > <h4>PAGES</h4> </li>
+              <li> <Link href={'/cart'}  >Shopping cart </Link></li>
+              <li> <Link href={'/checkout'}   >Checkout  </Link></li>
+              <li> <Link href={'/blog-details'}   >Blog Details </Link></li>
+              <li> <Link href={'/shop-details'}   >Shop Details  </Link></li>
+
+
+            </ul>
+            </div>
+         
 
     </div>
-    <div onClick={toggle}  className={`slide-menu-overlay ${ isOpen ? '' : 'closed-menu' } `} ></div>
+    <div onClick={()=>toggle('menu')}  className={`slide-menu-overlay ${ isOpen ? '' : 'closed-menu' } `} ></div>
     
 
           </div>
