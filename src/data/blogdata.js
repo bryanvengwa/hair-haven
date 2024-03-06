0b3b28334f9a4ba44febdc187bb9393685394bcc
bryanvengwa <bmvengwa@gmail.com>
2024-03-06 15:03:00 +0200

    added post 1

--- src/data/blogdata.js
+++ src/data/blogdata.js
@@ -39,14 +39,14 @@ const blogData = [
         exercept:"I share valuable insights on hair care, styling and archieving healthy, shiny hair based on extensive experience",
         date:"May 4, 2023",
         comment:"13",
-        paragraph:"",
-        title1:"",
-        title2:"",
-        tags:"",
-        author:"",
+        paragraph:"I write extensively about hair, covering essential aspects of care, styling, and establishing a solid foundation for your hair's health and shine. With years dedicated to working with diverse hair types, I've acquired extensive experience in addressing even the most challenging hair issues. Explore how my profound expertise can guide you on a transformative journey to achieving and maintaining lustrous, healthy hair that radiates confidence and beauty.",
+        title1:"Unlocking the Secrets to Healthy, Shiny Hair: A Comprehensive Guide",
+        title2:"Elevate Your Hair Game: A Deep Dive into Hair Care and Style Mastery",
+        tags:"I am passionate about empowering you with insights into optimal hair care, styling techniques, and the key elements for a foundation of vibrant, healthy hair. Drawing from extensive experience in the realm of hair, I've honed my skills to address the most intricate challenges. Embark on a journey with me as I share expert knowledge tailored to transform your hair. Discover the secrets to radiant, healthy locks that exude confidence and beauty. Your path to hair perfection starts here.",
+        author:"Amelia Rose Garcia",
         profile:"",
-        authorCategory:"",
-        profilePics:"" 
+        authorCategory:"Customer",
+        profilePic:"" 
     },
 
 
