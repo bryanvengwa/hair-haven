3bdf5e61443766af59c52ba86408b641b6bad1b9
bryanvengwa <bmvengwa@gmail.com>
2024-03-10 00:52:27 +0200

    added notification component

---
+++ src/components/Notification.js
@@ -0,0 +1,66 @@
+import React, { useMemo } from 'react';
+import {
+  RadiusBottomleftOutlined,
+  RadiusBottomrightOutlined,
+  RadiusUpleftOutlined,
+  RadiusUprightOutlined,
+} from '@ant-design/icons';
+import { Button, Divider, notification, Space } from 'antd';
+const Context = React.createContext({
+  name: 'Default',
+});
+const App = () => {
+  const [api, contextHolder] = notification.useNotification();
+  const openNotification = (placement) => {
+    api.info({
+      message: `Notification ${placement}`,
+      description: <Context.Consumer>{({ name }) => `Hello, ${name}!`}</Context.Consumer>,
+      placement,
+    });
+  };
+  const contextValue = useMemo(
+    () => ({
+      name: 'Ant Design',
+    }),
+    [],
+  );
+  return (
+    <Context.Provider value={contextValue}>
+      {contextHolder}
+      <Space>
+        <Button
+          type="primary"
+          onClick={() => openNotification('topLeft')}
+          icon={<RadiusUpleftOutlined />}
+        >
+          topLeft
+        </Button>
+        <Button
+          type="primary"
+          onClick={() => openNotification('topRight')}
+          icon={<RadiusUprightOutlined />}
+        >
+          topRight
+        </Button>
+      </Space>
+      <Divider />
+      <Space>
+        <Button
+          type="primary"
+          onClick={() => openNotification('bottomLeft')}
+          icon={<RadiusBottomleftOutlined />}
+        >
+          bottomLeft
+        </Button>
+        <Button
+          type="primary"
+          onClick={() => openNotification('bottomRight')}
+          icon={<RadiusBottomrightOutlined />}
+        >
+          bottomRight
+        </Button>
+      </Space>
+    </Context.Provider>
+  );
+};
+export default App;
