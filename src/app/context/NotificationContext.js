af3c61fba86e8c1d90ee975e4b3c0be583ab1ad2
bryanvengwa <bmvengwa@gmail.com>
2024-03-10 11:30:27 +0200

    added first parameter functionality to notify function

--- src/app/context/NotificationContext.js
+++ src/app/context/NotificationContext.js
@@ -8,27 +8,30 @@ export const NotificationContext = createContext();
 
 
 export const NotificationContextProvider = ({children})=>{
-    const [type, setType] = React.useState('error');
+    const [type, setType] = React.useState('info');
     const [placement, setPlacement] = React.useState('topStart');
     const toaster = useToaster();
   
 
-    const message = (
-        <Notification type={type} header={`${type}!`} closable>
-          <p>You have a {type} message, please check it.</p>
-          <hr />
-          <ButtonToolbar>
-            <Button appearance="primary">Ok</Button>
-            <Button appearance="default">Cancel</Button>
-          </ButtonToolbar>
-        </Notification>
-      );
+
+    const message = (infoType='info')=>{
+        return(
+            <Notification type={infoType ? infoType : 'info'} header={`${infoType}!`} closable>
+            <p>You have a {infoType} message, please check it.</p>
+            <hr />
+            {/* <ButtonToolbar>
+              <Button appearance="primary">Ok</Button>
+              <Button appearance="default">Cancel</Button>
+            </ButtonToolbar> */}
+          </Notification>
+        )
+    }
 
       
-const notify = () => toaster.push(message, { placement })
+const notify = () => toaster.push(message(), { placement })
 
 const contextData={
-    message,
+    // message,
     notify,
 }
 
