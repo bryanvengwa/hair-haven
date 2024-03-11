0b54f14bd7bfbbf0797520d87c400f06973da4a1
bryanvengwa <bmvengwa@gmail.com>
2024-03-11 06:25:26 +0200

    mobile responsive image on cartItem

--- src/components/CartImg.tsx
+++ src/components/CartImg.tsx
@@ -8,7 +8,7 @@ interface CartImgProps {
 export default function CartImg({ imageUrl }: CartImgProps) {
   return (
     <div className='cart-image' >
-      <Image src={imageUrl} width={200} height={200}  objectFit='cover' alt='product image' />
+      <Image src={imageUrl} width={200} height={200} className='img-fluid'  objectFit='cover' alt='product image' />
     </div>
   );
 }
