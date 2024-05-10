0def302c8fd789e0ea21c140f953eb4ca2e3e6a5
bryanvengwa <bmvengwa@gmail.com>
2024-05-10 06:36:32 +0200

    addedn LINK tag for NavigatioNLInk component

--- src/components/NavigationLink.tsx
+++ src/components/NavigationLink.tsx
@@ -1,19 +1,21 @@
+import Link from 'next/link'
 interface Props {
   children: React.ReactNode
   name: string
+  href?:string
 }
 
-const NavigationLink = ({ children, name }: Props) => {
+const NavigationLink = ({ children, name , href }: Props) => {
   return (
-    <a
-      href="#"
+    <Link
+      href={href ? href : '/'}
       className="flex p-1 rounded cursor-pointer stroke-[0.75] hover:stroke-neutral-100 stroke-neutral-900 text-black hover:text-neutral-400 place-items-center gap-3 hover:bg-neutral-700/30 transition-colors duration-100"
     >
       {children}
       <p className="text-inherit font-poppins overflow-clip whitespace-nowrap tracking-wide">
         {name}
       </p>
-    </a>
+    </Link>
   )
 }
 
